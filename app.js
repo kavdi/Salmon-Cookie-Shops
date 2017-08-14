36320c441fe623c58f0c667105e5d78455c2fcf2
kavdihodgson <kavdyjh@gmail.com>
2017-08-14 14:58:52 -0700

    got the stores in there

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+storeOne = {
+  name:'First and Pike',
+  minCust:'23',
+  maxCust:'65',
+  avgSale:'6.3'
+};
+
+storeTwo = {
+  name:'Seatac Airport',
+  minCust:'3',
+  maxCust:'24',
+  avgSale:'1.2'
+};
+
+storeThree = {
+  name:'Seattle Center',
+  minCust:'11',
+  maxCust:'38',
+  avgSale:'3.7'
+};
+
+storeFour = {
+  name:'Cap Hill',
+  minCust:'20',
+  maxCust:'38',
+  avgSale:'2.3'
+};
+
+storeFive = {
+  name:'Alki',
+  minCust:'2',
+  maxCust:'16',
+  avgSale:'4.6'
+};
+
+var shop = ['First and Pike', 'Seatac Airport', 'Seattle Center', 'Cap Hill', 'Alki']
