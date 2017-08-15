2156e79dbffa7abb1bab58ff7bd843beb4e2b000
kavdihodgson <kavdyjh@gmail.com>
2017-08-14 17:40:22 -0700

    got most of it done, need to finish js and do some css

--- app.js
+++ app.js
@@ -1,49 +1,72 @@
 'use strict';
 
-storeOne = {
+var storeOne = {
   name:'First and Pike',
   minCust:'23',
   maxCust:'65',
   avgSale:'6.3'
 };
 
-storeTwo = {
+var storeTwo = {
   name:'Seatac Airport',
   minCust:'3',
   maxCust:'24',
   avgSale:'1.2'
 };
 
-storeThree = {
+var storeThree = {
   name:'Seattle Center',
   minCust:'11',
   maxCust:'38',
   avgSale:'3.7'
 };
 
-storeFour = {
+var storeFour = {
   name:'Cap Hill',
   minCust:'20',
   maxCust:'38',
   avgSale:'2.3'
 };
 
-storeFive = {
+var storeFive = {
   name:'Alki',
   minCust:'2',
   maxCust:'16',
   avgSale:'4.6'
 };
 
-var shop = [storeOne, storeTwo, storeThree, storeFour, storeFive];
 var body = document.getElementsByTagName('body')[0];
-
+var newtable = document.createElement('table');
+var shop = [storeOne, storeTwo, storeThree, storeFour, storeFive];
 for (var i = 0; i < shop.lenght; i++);
-var ulTag = document.createElement('ul') * shop[i];
-body.apendchild(ulTag);
-var liTag = document.createElement('li') * shop[i];
-ul.apendchild(liTag);
-var customerPerHour = Math.floor(Math.random() * (shop[i].maxCust - shop[i].minCust[i]) + shop[i].minCust);
-console.log(customerPerHour);
-var cookiesPerHour = customersPerHour * shop[i].avgSale;
-console.log(cookiesPerHour);
+body.appendChild(newtable);
+
+for (var t = -1; t < 15; t++){
+  if (t === -1){
+    var time = 'Store Name';
+  } else if (t < 6){
+    var time = (t + 6) + 'am';
+  } else if (t === 6){
+    var time = '12pm';
+  } else if (t === 14){
+    var time = 'Total Sales'
+  } else {
+    var time = (t - 6) + 'pm';}
+  var timeHeading = document.createElement('th');
+  timeHeading.innerText = time;
+  newtable.appendChild(timeHeading);
+}
+
+for (var i = 0; i < shop.lenght; i++);{
+  var newRow = document.createElement('tr');
+  newRow.innerText = shop[i].name;
+  newtable.appendChild(newRow);
+  for (var t = 0; t < 15; t++){
+    if (t < 14){
+      var customerPerHour = Math.floor(Math.random() * (shop[i].maxCust - shop[i].minCust[i]) + shop[i].minCust);
+      console.log(customerPerHour);
+      var cookiesPerHour = Math.floor(customerPerHour * shop[i].avgSale);
+    }
+
+
+  console.log(cookiesPerHour);
