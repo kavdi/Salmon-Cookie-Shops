746f6aca7fb3b368ae095f4f0b580e2e712e88f9
kavdihodgson <kavdyjh@gmail.com>
2017-08-16 15:34:11 -0700

    finally getting somewhere

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var storeTwo = new stores('Seatac Airport', 3, 24, 1.2);
 var storeThree = new stores('Seattle Center', 11, 38, 3.7);
 var storeFour = new stores('Cap Hill', 20, 38, 2.3);
 var storeFive = new stores('Alki', 2, 16, 4.6);
-var locations = [storeOne, storeTwo, storeThree, storeFour, storeFive];
-var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var shops = ['First and Pike', 'Seatac Airport', 'Seattle Center', 'Cap Hill', 'Alki'];
+var storeHours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 var salesPerHour = [];
-
+var totalPerStore = [];
 function stores(name, minCust, maxCust, avgSale){
   this.name = name;
   this.minCust = minCust;
@@ -16,11 +16,21 @@ function stores(name, minCust, maxCust, avgSale){
   this.avgSale = avgSale;
   var cookiesPerHour = function(){
     for (var i = 0; i < storeHours.length; i++){
-      var customerPerHour = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-      var cookiesPerHour = Math.floor(customerPerHour * this.avgSale);
-      salesPerHour.push(cookiesPerHour);
+      if (i < storeHours.length - 1){
+        var customerPerHour = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+        var cookiesPerHour = Math.floor(customerPerHour * this.avgSale);
+        salesPerHour.push(cookiesPerHour);
+      }
+      else {
+        var cookiesPerStore = function() {
+          var cookies = 0;
+          for (var c = 0; c < salesPerHour.length; c++);
+          var cookies = cookies += salesPerHour[c];
+          totalPerStore.push(cookies);
+        };
+      }
 
-    };console.log(cookiesPerHour);
+    };
   };
 }
 
@@ -43,13 +53,20 @@ for (var t = -1; t < 15; t++){
   newtable.appendChild(timeHeading);
 }
 
-for (var i = 0; i < locations.length; i++){
+for (var i = 0; i < shops.length; i++){
   var row = document.createElement('tr');
+  row.innerText = shops[i];
+  for (var j = 0; j < storeHours.length; j++){
+    if (j <= storeHours.length - 1) {
+      var td = document.createElement('td');
+      td.innerText = salesPerHour[j];
+      row.appendChild(td);
+    }
+    else {
+      var total = document.createElement('td');
+      td.innerText = totalPerStore[j];
+    }
 
-  for (var j = 0; j < storeHours.lenght; j++){
-    var td = document.createElement('td');
-    td.innerText = locations[i].salesPerHour[j];
-    row.appendChild(td);
   }
   newtable.appendChild(row);
 }
